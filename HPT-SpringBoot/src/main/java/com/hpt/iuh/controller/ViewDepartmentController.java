74f77a6b26333b09cdd46d08d6d958593343089a
QuocHuy <quochuyp2004@gmail.com>
2024-10-16 19:24:59 +0700

    Add department ViewController

--- HPT-SpringBoot/src/main/java/com/hpt/iuh/controller/ViewDepartmentController.java
+++ HPT-SpringBoot/src/main/java/com/hpt/iuh/controller/ViewDepartmentController.java
@@ -7,30 +7,54 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller // This means that this class is a Controller that creates Views
-@RequestMapping("department")
+@RequestMapping("/department")
 public class ViewDepartmentController {
     public static final Logger logger = LoggerFactory.getLogger(ViewDepartmentController.class);
 
     @Autowired
     private DepartmentRepository departmentRepository;
 
-    @GetMapping
+    // View show all departments
+    @GetMapping("/")
     public String allDepartment() {
         return "departments/all";
     }
 
+    // View add a department
+    @GetMapping("/add")
+    public String addDepartment(Model model) {
+        model.addAttribute("department", new Department());
+        return "departments/add";
+    }
+
+    // View Method add department
+    @PostMapping("/add")
+    public String addDepartment(@ModelAttribute("department") Department department, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "departments/add";
+        }
+        try {
+            departmentRepository.save(department);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Error saving department: " + e.getMessage());
+            return "departments/add";
+        }
+        return "redirect:/";
+    }
+
+    // View update a department
     @GetMapping("/update/{id}")
     public String updateDepartment(@PathVariable("id") String id, Model model) {
         logger.info("Updating User with id {}", id);
         Department dp = departmentRepository.findById(id).orElse(null);
         if (dp == null) {
             logger.error("Unable to update. User with id {} not found.", id);
-            return "error/404"; // Trang lỗi nếu không tìm thấy department
+            throw new RuntimeException("Unable to update. User with id " + id + " not found.");
         }
         model.addAttribute("department", dp);
         return "departments/update";
