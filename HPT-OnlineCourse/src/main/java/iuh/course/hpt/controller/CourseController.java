84b66db758a263420692fc283f7561194a1391ad
QuocHuy <quochuyp2004@gmail.com>
2024-11-24 00:27:50 +0700

    course detail

--- HPT-OnlineCourse/src/main/java/iuh/course/hpt/controller/CourseController.java
+++ HPT-OnlineCourse/src/main/java/iuh/course/hpt/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -26,17 +27,14 @@ public class CourseController {
         return "list-course";
     }
 
-    // course detail page
+    // retrieve a course by id
     @GetMapping("/course/{id}")
-    public String getACourse(@PathVariable("id") Long id, Model model) {
+    public String courseDetail(@PathVariable Long id, Model model) {
+        // get course by id
         Course course = courseService.getById(id);
 
-        if (course == null) {
-            model.addAttribute("error", "Không tìm thấy khóa học");
-            return "course-detail";
-        }
-
-        model.addAttribute("title", "Khóa học");
+        model.addAttribute("title", "Chi tiết khóa học");
+        model.addAttribute("success", "Chào mừng bạn đến với khóa học");
         model.addAttribute("course", course);
         return "course-detail";
     }
