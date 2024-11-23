23aca537b572d28ddead7258cb077a75a8cc6711
Feliz Vu <darklight.trongphuc@yahoo.com>
2024-11-23 17:12:53 +0700

    Custom alert

--- HPT-OnlineCourse/src/main/java/iuh/course/hpt/controller/AdminController.java
+++ HPT-OnlineCourse/src/main/java/iuh/course/hpt/controller/AdminController.java
@@ -2,6 +2,7 @@ package iuh.course.hpt.controller;
 
 import iuh.course.hpt.entity.Category;
 import iuh.course.hpt.service.interfaces.CategoryService;
+import iuh.course.hpt.utility.Utils;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,72 +15,73 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Data
 @Controller
 public class AdminController {
-    
+
     @Autowired
     private CategoryService categoryService;
-    
+
     @GetMapping("/admin/category")
-    public String category(Model model) {
+    public String category(Model model, @ModelAttribute("error") String error, @ModelAttribute("success") String success) {
         model.addAttribute("title", "Quản lý danh mục");
-        
+
         // get all categories
         model.addAttribute("categories", categoryService.getAll());
-        
+
+        // create new category
+        model.addAttribute("category", new Category());
+
         return "admin/category";
     }
-    
+
     // Create new category
-    @RequestMapping(value = "/admin/category/create", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/category", method = RequestMethod.POST)
     public String createCategory(@ModelAttribute("category") Category category, Model model) {
-        
+
         // check category is existed
         boolean isCategoryExisted = categoryService.isCategoryExisted(category.getCategoryName());
-        
+
         if (isCategoryExisted) {
-            model.addAttribute("error", "Danh mục đã tồn tại");
-            return "admin/category";
+            return "redirect:/admin/category?error=" + Utils.getInstance().encodeUrlSafe("Danh mục đã tồn tại");
         }
-        
+
         Category result = categoryService.save(category);
         if (result != null) {
-            model.addAttribute("success", "Thêm danh mục thành công");
+            return "redirect:/admin/category?success=" + Utils.getInstance().encodeUrlSafe("Tạo danh mục thành công");
         } else {
-            model.addAttribute("error", "Thêm danh mục thất bại");
+            return "redirect:/admin/category?error=" + Utils.getInstance().encodeUrlSafe("Tạo danh mục thất bại");
         }
-        
-        return "admin/category";
+
     }
-    
+
     // Edit category
     @RequestMapping(value = "/admin/category/update", method = RequestMethod.POST)
     public String editCategory(@ModelAttribute("category") Category category, Model model) {
-        
+
         // check category is existed
         boolean isCategoryExisted = categoryService.isCategoryExisted(category.getCategoryName());
-        
+
         if (isCategoryExisted) {
             model.addAttribute("error", "Danh mục đã tồn tại");
             return "admin/category";
         }
-        
+
         Category result = categoryService.save(category);
         if (result != null) {
             model.addAttribute("success", "Cập nhật danh mục thành công");
         } else {
             model.addAttribute("error", "Cập nhật danh mục thất bại");
         }
-        
+
         return "admin/category";
     }
-    
+
     // Delete category
     @RequestMapping(value = "/admin/category/delete", method = RequestMethod.GET)
     public String deleteCategory(@ModelAttribute("category") Category category, Model model) {
-        
+
         categoryService.deleteById(category.getId());
-        
+
         model.addAttribute("success", "Xóa danh mục thành công");
-        
+
         return "admin/category";
     }
 }
